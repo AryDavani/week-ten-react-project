15f4a04eb7af033190fb6bb8caa2f8a787466de8
Ary Davani <arydavani@gmail.com>
2017-07-24 11:37:23 -0400

    reroute plus boilerplate

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../styles/App.css';
+
+import NavBar from './NavBar';
+import PlayList from './PlayList';
+import PlayListForm from './PlayListForm';
+import PlayListItem from './PlayListItem';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container-fluid">
+        <h1>is this thing working</h1>
       </div>
     );
   }
